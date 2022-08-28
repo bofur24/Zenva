ae01ca4ef22bb378166858ee4ed81a6703540176
keithheffington <keith.heffington@gmail.com>
2022-08-27 22:31:26 -0500

    DOM set up and Retrieve elements from DOM

---
+++ Full-Stack-Web-Development-Mini-Degree/JavaScript-DOM/js/intro-to-DOM.js
@@ -0,0 +1,25 @@
+
+
+// Retrieve elements from the document 5 different ways.
+
+// let class1 = document.getElementsByClassName('class1');
+// let button1 = document.getElementById('button1');
+// let divs = document.getElementsByTagName('DIV');
+// let first = document.querySelector('.class1');
+// let all = document.querySelectorAll('.class1');
+// let div1 = document.getElementById('div1');
+// let innerDivs = div1.getElementsByClassName('class1');
+// alert(innerDivs[0].innerHTML);
+
+
+//alert(divs[0].innerHTML);
+//alert(button1.innerHTML);
+//alert(class1[0].innerHTML);
+//alert(class1[1].innerHTML);
+
+//Retrieve and edit the elements
+
+
+
+
+
