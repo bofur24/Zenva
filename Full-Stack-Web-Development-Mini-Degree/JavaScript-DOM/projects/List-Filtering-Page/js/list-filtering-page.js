4d4818f36c7e5cc4ad972b95148398f87a254c5a
bofur24 <dransiktolkien@yahoo.com>
2022-08-29 16:19:13 -0500

    Completed the List Filtering Page Project

--- Full-Stack-Web-Development-Mini-Degree/JavaScript-DOM/projects/List-Filtering-Page/js/list-filtering-page.js
+++ Full-Stack-Web-Development-Mini-Degree/JavaScript-DOM/projects/List-Filtering-Page/js/list-filtering-page.js
@@ -1,7 +1,22 @@
 let adventureList = [];
 
-let adventures = document.getElementsByClassName('adventure');
+let adventures = document.getElementsByClassName('adventures');
 for (let i = 0; i < adventures.length; i++) {
     let adventureName = adventures[i].innerHTML;
     adventureList.push(adventureName);
 }
+
+let searchBar = document.getElementById('search-bar');
+
+searchBar.addEventListener('keyup', function(event) {
+    let text = searchBar.value;
+    
+    for (let i = 0; i < adventures.length; i++) {
+        if (adventureList[i].indexOf(text) < 0){    
+            adventures[i].style.display = 'none';
+        } else {
+            adventures[i].style.display = 'block';
+        }
+    }    
+    
+});
