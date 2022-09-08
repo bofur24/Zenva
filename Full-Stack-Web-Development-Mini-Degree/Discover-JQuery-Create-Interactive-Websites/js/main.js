71c8d7c246bcb6a9605096d2e1a0adb7e087818d
keithheffington <keith.heffington@gmail.com>
2022-09-08 11:32:38 -0500

    JQuery setup

---
+++ Full-Stack-Web-Development-Mini-Degree/Discover-JQuery-Create-Interactive-Websites/js/main.js
@@ -0,0 +1,13 @@
+// Check to see if the document is ready
+// $(document).ready(function() {
+//     console.log("We are ready!")
+// }); 
+
+$(document).ready(function() {
+    //$('body').text('Hello world!') text only to the body
+    // $('body').html('<strong>Hello World!</strong>'); add html tag with text to the body
+    //$('#fire').addClass('highlight'); looks for the selector of fire added the class of highlight
+    //$('.solid').addClass('highlight') looks for the selector of solid and adds the class of highlight
+    //$('.solid, .non-solid').addClass('highlight'); looks for both selectors and adds the class of highlight
+    
+});     
