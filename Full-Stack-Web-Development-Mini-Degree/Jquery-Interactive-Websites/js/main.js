f65892e8b6669a800ea235f2630e3fd4f5a7ab68
bofur24 <dransiktolkien@yahoo.com>
2022-09-28 09:17:03 -0500

    Working with adding Classes

--- Full-Stack-Web-Development-Mini-Degree/Jquery-Interactive-Websites/js/main.js
+++ Full-Stack-Web-Development-Mini-Degree/Jquery-Interactive-Websites/js/main.js
@@ -7,6 +7,18 @@
 //     $('body').html('<ul><li>Time</li></ul>') // overwrites hello world
 // });
 
-$(document).ready(function() {
-    $('#fire').addClass('highlight');
+// $(document).ready(function() {
+//     $('#fire').addClass('highlight');
+// });
+
+// $(document).ready(function() {
+//     $('.non-solid').addClass('highlight');
+// });
+
+// $(document).ready(function() {
+//     $('#container >>> .non-solid').addClass('highlight'); // > are levels in the hiarchy >level 1 H4 and UL, >> Level 2 li >>> Level 3 li
+// });
+
+$(document).ready(function(){
+    $('#container input').addClass('highlight');
 });
